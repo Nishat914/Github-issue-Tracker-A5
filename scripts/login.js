38e978476b7da41d07d6fb44d946d27897e6929b
Nishat914 <jahannishat914@gmail.com>
2026-03-07 11:48:05 +0600

    active sign in btn

---
+++ scripts/login.js
@@ -0,0 +1,16 @@
+document.getElementById('SignInBtn').addEventListener('click',function(){
+    const inputUsername=document.getElementById('inputUsername');
+    const inputPassword=document.getElementById('inputPassword');
+    
+    const userName=inputUsername.value;
+    const password=inputPassword.value;
+
+    if(userName==='admin' && password==='admin123'){
+        window.location.assign('./home.html');
+    }
+    else{
+        alert('Wrong info!');
+        return;
+    }
+
+})
